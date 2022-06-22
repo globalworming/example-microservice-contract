38f05b540b990f9cbfc681a5fb226bf41638c330
globalworming <andreas.worm@headissue.com>
2022-06-22 17:09:11 +0200

    start ui and putting everything together

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
+
+  const [user, setUser] = useState('')
+  const [paymentSuccess, setPaymentSuccess] = useState(false)
+  const [message, setMessage] = useState(undefined)
+
+  const handleChange = ev => {
+    ev.preventDefault()
+    setUser(ev.currentTarget.value)
+  }
+
+  const buyCoffee = () => {
+    axios
+        .post(`/pay`, {user: user, amount: 5.99})
+        .then((result) => {
+          setPaymentSuccess(true)
+          setMessage(result.data);
+        })
+        .catch((err) => console.log("error", err));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>user <input type={"text"} value={user} aria-label="user-name" onChange={handleChange}/></p>
+          <p>
+            <button onClick={buyCoffee} aria-label="buy-coffee">buy coffee</button>
+            <button aria-label="buy-car">buy car</button>
+          </p>
+          { paymentSuccess && <p aria-label="payment-success"><span aria-label="message">{message}</span></p>}
+        </header>
+      </div>
   );
 }
 
--- shop/src/App.test.js
+++ shop/src/App.test.js
@@ -1,9 +1,43 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react'
+import nock from 'nock'
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
+test('where low risk payment is confirmed', async () => {
+  render(<App/>)
+
+  nock("http://localhost")
+      .post('/pay', {user: 'Athena', amount: 5.99})
+      .once()
+      .reply(200, "OK");
+
+  fireEvent.change(screen.getByLabelText('user-name'), {target: {value: 'Athena'}})
+  fireEvent.click(screen.getByLabelText('buy-coffee'))
+
+  await waitFor(() => screen.getByLabelText('payment-success'))
+
+  expect(screen.getByLabelText('message')).toHaveTextContent('OK')
+})
+
+test('where high risk payment needs 2FA', async () => {
+  render(<App/>)
+
+  nock("http://localhost")
+      .post('/pay', {user: 'Riko', amount: 20000})
+      .once()
+      .reply(200, "2fa required");
+
+  fireEvent.change(screen.getByLabelText('user-name'), {target: {value: 'Riko'}})
+  fireEvent.click(screen.getByLabelText('buy-car'))
+
+  await waitFor(() => screen.getByLabelText('2fa-required'))
+
+  expect(screen.getByLabelText('message')).toHaveTextContent('OK')
+})

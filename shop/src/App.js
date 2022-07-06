05ba1975bd58665495afda6d1d724db89d7ec23a
globalworming <andreas.worm@headissue.com>
2022-07-06 21:49:26 +0200

    add name input, check high risk request

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,18 +4,21 @@ import axios from "axios";
 import {useState} from "react";
 
 function App() {
-
   const [paymentSuccess, setPaymentSuccess] = useState(undefined)
   const [message, setMessage] = useState(undefined)
+  const [errorMessage, setErrorMessage] = useState(undefined)
+  const [userName, setUserName] = useState("")
 
-  const sendLowRiskRequest = () => {
+  const sendPaymentRequest = (amount) => {
     axios
-        .post(`/pay`, {user: "Athena", amount: 5.99})
+        .post(`/pay`, {user: userName, amount: amount})
         .then((result) => {
           setPaymentSuccess(true)
           setMessage(result.data);
         })
-        .catch((err) => console.log("error", err));
+        .catch((err) => {
+          setErrorMessage(err.response.data)
+        });
   }
 
   return (
@@ -23,8 +26,11 @@ function App() {
         <header className="App-header">
           <a href="/">learn react</a>
           <img src={logo} className="App-logo" alt="logo"/>
-          <button aria-label="perform low risk request" onClick={sendLowRiskRequest}>low risk request</button>
+          <input aria-label="user name input" onChange={(e) => setUserName(e.target.value)} value={userName}/>
+          <button aria-label="buy car" onClick={() => sendPaymentRequest(10000.99)}>buy car</button>
+          <button aria-label="buy coffee" onClick={() => sendPaymentRequest(5.99)}>buy coffee</button>
           {paymentSuccess && <span aria-label="message">{message}</span>}
+          {errorMessage && <span aria-label="error message">{errorMessage}</span>}
         </header>
       </div>
   );
--- shop/src/App.test.js
+++ shop/src/App.test.js
@@ -4,26 +4,57 @@ import nock from 'nock'
 import {render, fireEvent, waitFor, screen} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
+// TODO refactor to given and steps
+function setupSystemUnderTest() {
+  render(<App/>)
+}
 
 test('where low risk payment is confirmed successfully', async () => {
-  render(<App/>)
-  Given.paymentServiceAnswersOk()
-  clickBuy();
+  setupSystemUnderTest();
+  GivenMock.paymentServiceAnswersOk()
+  enterName("Athena")
+  buyCofee();
   await waitFor(() => successMessage())
   expect(successMessage()).toHaveTextContent('payment successful')
 })
 
-const Given = {
+test('where high risk payment shows 2fa needed', async () => {
+  setupSystemUnderTest();
+  GivenMock.paymentServiceAnswersSecondFactorNeeded()
+  enterName("Riko")
+  buyCar();
+  await waitFor(() => twoFactorRequired())
+  expect(twoFactorRequired()).toHaveTextContent('2fa required')
+})
+
+const GivenMock = {
   paymentServiceAnswersOk : () => {
     nock("http://localhost")
         .post('/pay', {user: 'Athena', amount: 5.99})
         .once()
         .reply(200, "payment successful");
+  },
+
+  paymentServiceAnswersSecondFactorNeeded : () => {
+    nock("http://localhost")
+        .post('/pay', {user: 'Riko', amount: 10000.99})
+        .once()
+        .reply(400, "2fa required");
   }
 }
 
-const clickBuy = () => {
-  fireEvent.click(screen.getByLabelText("perform low risk request"))
+const buyCofee = () => {
+  fireEvent.click(screen.getByLabelText("buy coffee"))
+};
+
+const buyCar = () => {
+  fireEvent.click(screen.getByLabelText("buy car"))
+};
+
+const enterName = (name) => {
+  let byLabelText = screen.getByLabelText("user name input");
+  fireEvent.change(byLabelText,  {target: {value: name}}  )
 };
 
 const successMessage = () => screen.getByLabelText('message');
+const twoFactorRequired = () => screen.getByLabelText('error message');
